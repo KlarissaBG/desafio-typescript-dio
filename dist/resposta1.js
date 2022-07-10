f1360237b45b7c1a1a9d55a75941537458a41e0c
KlarissaBG <aklaragoncalves@gmail.com>
2022-07-10 19:32:51 -0300

    primiro commit

---
+++ dist/resposta1.js
@@ -0,0 +1,10 @@
+/*Como podemos rodar isso em um arquivo .ts sem causar erros?
+
+let employee = {};
+employee.code = 10;
+employee.name = "John"; */
+let funcionario = {
+    codigo: 10,
+    nome: 'John',
+};
+//# sourceMappingURL=resposta1.js.map
